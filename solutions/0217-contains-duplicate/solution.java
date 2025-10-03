bab1454b077d3724be4fc2203797ac062d628a71
Shashwat-dev29 <shashwatmishra425@gmail.com>
2025-10-03 10:48:45 +0000

    Sync LeetCode submission - Contains Duplicate - Runtime - 19 ms (36.13%), Memory - 58.2 MB (99.92%)

---
+++ solutions/0217-contains-duplicate/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      Arrays.sort(nums);
+        for(int x=0;x<nums.length-1;x++)
+        {
+            if(nums[x]==nums[x+1])
+            {
+              return true;
+            }
+        }
+       return false; 
+    }
+}
