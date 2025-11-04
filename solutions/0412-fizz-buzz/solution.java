7a340a968dd47fd28b08e13757662f13ee657bce
Shashwat-dev29 <shashwatmishra425@gmail.com>
2025-11-04 07:27:51 +0000

    Sync LeetCode submission - Fizz Buzz - Runtime - 1 ms (99.85%), Memory - 46.9 MB (45.98%)

---
+++ solutions/0412-fizz-buzz/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String>list=new ArrayList<>();
+        for(int x=1;x<=n;x++)
+        {
+            if(x%3==0&&x%5==0)
+            {
+                list.add("FizzBuzz");
+            }
+            else if(x%3==0)
+            {
+                list.add("Fizz");
+            }
+            else if(x%5==0)
+            {
+                list.add("Buzz");
+            }
+            else
+            {
+                list.add(String.valueOf(x));
+            }
+        }
+        return list;
+    }
+}
