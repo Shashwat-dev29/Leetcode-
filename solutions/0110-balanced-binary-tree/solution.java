e5eff5cf7308810bc0ece4b255afc00b649d52dd
Shashwat-dev29 <shashwatmishra425@gmail.com>
2025-11-21 05:03:58 +0000

    Sync LeetCode submission - Balanced Binary Tree - Runtime - 1 ms (31.70%), Memory - 45.3 MB (89.03%)

---
+++ solutions/0110-balanced-binary-tree/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+    
+      if(root==null)
+      {
+        return true;
+      }
+      int lefthigh=height(root.left);
+      int righthigh=height(root.right);
+      if(Math.abs(lefthigh-righthigh)>1)
+      {
+        return false;
+      }
+      return isBalanced(root.left)&&isBalanced(root.right);
+    }
+    int height(TreeNode root) {
+    if (root == null) return -1; 
+    int left = height(root.left);
+    int right = height(root.right);
+
+    return Math.max(left, right) + 1;
+}
+}
