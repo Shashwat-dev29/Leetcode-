a9e09f86cd7c2dca6c62227680e34f11c9c6ecef
Shashwat-dev29 <shashwatmishra425@gmail.com>
2026-03-30 19:37:34 +0000

    Sync LeetCode submission - Search a 2D Matrix - Runtime - 0 ms (100.00%), Memory - 43.9 MB (62.01%)

--- solutions/0074-search-a-2d-matrix/solution.java
+++ solutions/0074-search-a-2d-matrix/solution.java
@@ -1,39 +1,28 @@
 import java.util.Scanner;
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean flag=false;
-        for(int y=0;y<matrix[0].length;y++)
+       
+        int f=0;
+        int l=(matrix[0].length*matrix.length)-1;
+        while(f<=l)
         {
-        for(int x=0;x<matrix.length;x++)
-        {
-            if(target==matrix[x][y])
-            {
-                flag=true;
-            }
+          int mid=f+(l-f)/2;
+          if(matrix[mid/matrix[0].length][mid%matrix[0].length]==target)
+          {
+            return true;
+          }
+          else if(matrix[mid/matrix[0].length][mid%matrix[0].length]>target)
+          {
+               l=mid-1;
+          }
+          else if(matrix[mid/matrix[0].length][mid%matrix[0].length]<target)
+          {
+         
+            f=mid+1;
+          }
         }
-        }
-      return flag;
+      return false;
       
     }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number of rows of a matrix");
-        int r= sc.nextInt();
-        System.out.println("enter the number of columns of a matrix");
-        int c=  sc.nextInt();
-        int arr[][]=new int[r][c];
-         System.out.println("enter the number to be searched");
-        int targ=  sc.nextInt();
-        Solution obj= new Solution();
-        boolean fl= obj.searchMatrix(arr,targ);
-        if(fl==true)
-        {
-            System.out.println("true");
-        }
-        else
-        {
-            System.out.println(fl);
-        }
-    }
+    
 }
