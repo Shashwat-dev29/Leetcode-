e108df5bdc844c93c5495da7ce572d61564e863f
Shashwat-dev29 <shashwatmishra425@gmail.com>
2026-09-11 03:29:17 +0530

    Time: 5 ms (72.21%) | Memory: 43.6 MB (41.62%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        // 2. Reverse the array in-place
+        int left = 0;
+        int right = citations.length - 1;
+        while (left < right) {
+            int temp = citations[left];
+            citations[left] = citations[right];
+            citations[right] = temp;
+            left++;
+            right--;
+        }
+        int count=0;
+        for(int x=0;x<citations.length;x++)
+        {
+            if(citations[x]<=x)
+            {
+                count ++;
+            }
+        }
+        return citations.length-count;
+    }
+}
